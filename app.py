0674cad7b9b6842d6f93299cb9e551450dbc117f
jjasmin123 <43469860+jjasmin123@users.noreply.github.com>
2023-06-12 20:34:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,5 @@ async def predict_route(text):
     
 
 if __name__=="__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    #uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="127.0.0.1", port=8080)
